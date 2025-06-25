71de4a9af9d85d0f1d87d71c3fb259801fe7e583
MadeByZino <jh990517@naver.com>
2025-06-25 14:25:06 +0900

    Initial commit

---
+++ src/main/java/com/zino/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.zino.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hi() {
+        return "hi";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
